5ce648995095bde614f9c4041b8da1d78a3e97ad
Kane Basu <Kanerox@users.noreply.github.com>
2026-08-14 18:13:57 +0530

    Enable responsive stylesheet

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
 import App from "./App.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <>
